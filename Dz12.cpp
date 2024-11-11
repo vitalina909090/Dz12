98e2aef7d091a727cebe293f3574cc1542eeac47
Legeza Vitalina <legezavitalina06@gmail.com>
2024-11-11 16:26:16 +0200

    додано виключення

--- Dz12.cpp
+++ Dz12.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream> 
+﻿#include <iostream>
 #include <Windows.h>
 using namespace std;
 
@@ -10,19 +10,26 @@ protected:
 
 public:
     Transport(double speedP, double costP, double expensesP)
-        : speed(speedP), cost(costP), expenses(expensesP) {}
+        : speed(speedP), cost(costP), expenses(expensesP) {
+        if (speed <= 0 || cost < 0 || expenses < 0) {
+            throw "Некоректний ввід";
+        }
+    }
 
     double time(double length) {
+        if (length <= 0) throw "Відстань має бути більше нуля";
         return length / speed;
     }
 
     virtual void costDetermination(double length) {
+        if (length <= 0) throw "Відстань має бути більше нуля";
         double totalCost = (length / 100.0) * expenses * cost;
         cout << "Вартість = " << totalCost << " грн" << endl;
     }
 
     virtual void print(double length) {
-        cout << "Швидкість: " << speed << " км/год, " << "Вартість бензину на 100 км: " << cost << " грн " << endl;
+        cout << "Швидкість: " << speed << " км/год, "
+            << "Вартість бензину на 100 км: " << cost << " грн " << endl;
     }
 
     virtual void passengers(int numPassengers) {}
@@ -37,23 +44,31 @@ private:
 
 public:
     Car(double speedP, double costP, double expensesP, int maxPassengersP, double maxPayloadP, string fuelTypeP)
-        : Transport(speedP, costP, expensesP), maxPassengers(maxPassengersP), maxPayload(maxPayloadP), fuelType(fuelTypeP) {}
-
-    void passengers(int numPassengers, double length) {
-        if (numPassengers <= maxPassengers) {
-            costDetermination(length);
+        : Transport(speedP, costP, expensesP), maxPassengers(maxPassengersP), maxPayload(maxPayloadP), fuelType(fuelTypeP) {
+        if (maxPassengers <= 0 || maxPayload < 0) {
+            throw "Некоректний ввід для авто";
         }
     }
 
-    void payload(double cargo, double length) {
-        if (cargo <= maxPayload) {
-            costDetermination(length);
+    void passengers(int numPassengers, double length) {
+        if (numPassengers > maxPassengers) {
+            throw "Перевищена максимальна кількість пасажирів";
         }
+        costDetermination(length);
+    }
+
+    void payload(double cargo, double length) {
+        if (cargo > maxPayload) {
+            throw "Перевищена максимальна вага вантажу";
+        }
+        costDetermination(length);
     }
 
     void print(double length) {
         Transport::print(length);
-        cout << "Макс. пасажири: " << maxPassengers << ", " << "Макс. вантаж: " << maxPayload << " кг, " << "Тип пального: " << fuelType << endl;
+        cout << "Макс. пасажири: " << maxPassengers << ", "
+            << "Макс. вантаж: " << maxPayload << " кг, "
+            << "Тип пального: " << fuelType << endl;
         cout << "Час: " << time(length) << " год" << endl;
         costDetermination(length);
     }
@@ -66,23 +81,22 @@ private:
 
 public:
     Bicycle(double speedP, double maxWeightBasketP, bool hasBellP)
-        : Transport(speedP, 0, 0), maxWeightBasket(maxWeightBasketP), hasBell(hasBellP) {}
-
-    void passengers(int numPassengers) {
-        if (numPassengers == 1) {
-            return;
+        : Transport(speedP, 0, 0), maxWeightBasket(maxWeightBasketP), hasBell(hasBellP) {
+        if (maxWeightBasket < 0) {
+            throw "Некоректна максимальна вага кошика";
         }
     }
 
     void payload(double cargo) {
-        if (cargo <= maxWeightBasket) {
-            return;
+        if (cargo > maxWeightBasket) {
+            throw "Перевищена максимальна вага вантажу для кошика";
         }
     }
 
     void print(double length) {
         Transport::print(length);
-        cout << "Макс. вага кошика: " << maxWeightBasket << " кг, " << "Наявність дзвоника: " << (hasBell ? "Так" : "Ні") << endl;
+        cout << "Макс. вага кошика: " << maxWeightBasket << " кг, "
+            << "Наявність дзвоника: " << (hasBell ? "Так" : "Ні") << endl;
         cout << "Час: " << time(length) << " год" << endl;
         costDetermination(length);
     }
@@ -97,21 +111,14 @@ private:
 
 public:
     Cart(double speedP, double maxKilogramP, int horsePowerP, string materialP, double foodCostP)
-        : Transport(speedP, 0, 0), maxKilogram(maxKilogramP), horsePower(horsePowerP), material(materialP), foodCost(foodCostP) {}
-
-    void passengers(int numPassengers) {
-        if (numPassengers <= horsePower * 2) {
-            return;
-        }
-    }
-
-    void payload(double cargo) {
-        if (cargo <= maxKilogram) {
-            return;
+        : Transport(speedP, 0, 0), maxKilogram(maxKilogramP), horsePower(horsePowerP), material(materialP), foodCost(foodCostP) {
+        if (maxKilogram < 0 || horsePower <= 0 || foodCost < 0) {
+            throw "Некоректний ввід для возу!";
         }
     }
 
     void costDetermination(double length) {
+        if (length <= 0) throw "Відстань має бути більше нуля";
         double totalCost = (length / 100.0) * expenses * cost + foodCost;
         cout << "Вартість = " << totalCost << " грн (включно з їжею)" << endl;
     }
@@ -131,20 +138,27 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    Car car(90, 6, 8, 4, 300, "Бензин");
-    Bicycle bicycle(15, 3, true);
-    Cart cart(10, 500, 2, "Дерево", 250);
+    try {
+        Car car(90, 6, 8, 4, 300, "Бензин");
+        Bicycle bicycle(15, 3, true);
+        Cart cart(10, 500, 2, "Дерево", 250);
 
-    double length;
-    cout << "Введіть відстань для перевезення (км): ";
-    cin >> length;
+        double length;
+        cout << "Введіть відстань для перевезення (км): ";
+        cin >> length;
 
-    cout << "Автомобіль:\n";
-    car.print(length);
-    cout << "\nВелосипед:\n";
-    bicycle.print(length);
-    cout << "\nВіз:\n";
-    cart.print(length);
+        if (length <= 0) throw "Відстань має бути більше нуля";
+
+        cout << "Автомобіль:\n";
+        car.print(length);
+        cout << "\nВелосипед:\n";
+        bicycle.print(length);
+        cout << "\nВіз:\n";
+        cart.print(length);
+    }
+    catch (const char* error) {
+        cerr << "Помилка: " << error << endl;
+    }
 
     return 0;
 }
